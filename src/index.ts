7bbbc5cc4fadc4f4c51b7ecf04bf217bb38cab1b
Pasi Kemppainen <pasi.kemppainen@gmail.com>
2020-09-10 22:10:21 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,400 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as qs from 'querystring';
+import * as Moment from 'moment';
+import * as _ from 'lodash';
+import { escape, toNanoDate } from 'influx';
+
+export interface ParserOptions {
+	dateFormat?: any;
+	whitelist?: string[]; // list of fields allowed to be in the filter
+	blacklist?: string[]; // list of fields disallowed to be in the filter
+	casters?: { [key: string]: (val: string) => any };
+	castParams?: { [key: string]: string };
+	measurements?: string;
+	// rename the keys
+	fieldsKey?: string;
+	sortKey?: string;
+	limitKey?: string;
+	filterKey?: string;
+	aggregateKey?: string;
+}
+
+export interface QueryOptions {
+	filter: any;
+	sort?: string | Record<string, any>;
+	limit?: string;
+	fields?: string | Record<string, any>;
+	aggregate: { groupBy: string; agg: string };
+}
+
+export class InfluxDbQueryParser {
+	private readonly _defaultDateFormat = [Moment.ISO_8601];
+
+	private readonly _builtInCaster = {
+		string: val => String(val),
+		number: val => Number(val),
+		date: val => {
+			const shortcuts = {
+				startOfYear: (key, mod) =>
+					Moment.utc()
+						.startOf('year')
+						.add(Number(mod || '0'), 'years'),
+				endOfYear: (key, mod) =>
+					Moment.utc()
+						.endOf('year')
+						.add(Number(mod || '0'), 'years'),
+				startOfQuarter: (key, mod) =>
+					Moment.utc()
+						.startOf('quarter')
+						.add(Number(mod || '0'), 'quarters'),
+				endOfQuarter: (key, mod) =>
+					Moment.utc()
+						.endOf('quarter')
+						.add(Number(mod || '0'), 'quarters'),
+				startOfMonth: (key, mod) =>
+					Moment.utc()
+						.startOf('month')
+						.add(Number(mod || '0'), 'months'),
+				endOfMonth: (key, mod) =>
+					Moment.utc()
+						.endOf('month')
+						.add(Number(mod || '0'), 'months'),
+				startOfWeek: (key, mod) =>
+					Moment.utc()
+						.startOf('isoWeek')
+						.add(Number(mod || '0'), 'weeks'),
+				endOfWeek: (key, mod) =>
+					Moment.utc()
+						.endOf('isoWeek')
+						.add(Number(mod || '0'), 'weeks'),
+			};
+			//const [, key, mod] = /(^[a-zA-Z]+$)|^([a-zA-Z]+):(.+)$/.exec(val);
+			const matches = val.match(/^([a-zA-Z]+):?([0-9-.]*)$/);
+			if (matches) {
+				if (shortcuts[matches[1]]) {
+					return shortcuts[matches[1]](matches[1], matches[2])
+						.toDate()
+						.toISOString();
+				} else {
+					throw new Error(`Unknown date shortcut: ${matches[1]}`);
+				}
+			}
+			const m = Moment.utc(val, this._options.dateFormat);
+			if (m.isValid()) {
+				return m.toISOString();
+			} else {
+				throw new Error(`Invalid date string: ${val}`);
+			}
+		},
+	};
+
+	private readonly _operators = [
+		{ operator: 'fields', method: this.castFields, defaultKey: 'fields' },
+		{ operator: 'sort', method: this.castSort, defaultKey: 'sort' },
+		{ operator: 'limit', method: this.castLimit, defaultKey: 'limit' },
+		{ operator: 'filter', method: this.castFilter, defaultKey: 'filter' },
+		{ operator: 'aggregate', method: this.castAggregate, defaultKey: 'aggregate' },
+	];
+
+	constructor(private _options: ParserOptions = {}) {
+		// add default date format as ISO_8601
+		this._options.dateFormat = _options.dateFormat || this._defaultDateFormat;
+
+		// add builtInCaster
+		this._options.casters = Object.assign(this._builtInCaster, _options.casters);
+
+		// build blacklist
+		this._options.blacklist = _options.blacklist || [];
+		this._operators.forEach(({ operator, defaultKey }) => {
+			this._options.blacklist.push(this._options[`${operator}Key`] || defaultKey);
+		});
+	}
+
+	/**
+	 * parses query string/object to QueryOptions
+	 * @param {string | Object} query
+	 * @param {Object} [context]
+	 * @return {string}
+	 */
+	parse(query: string | Record<string, any>): QueryOptions {
+		const params = _.isString(query) ? qs.parse(query) : query;
+		const options = this._options;
+		const result = {
+			fields: '*',
+		};
+
+		this._operators.forEach(({ operator, method, defaultKey }) => {
+			const key = options[`${operator}Key`] || defaultKey;
+			const value = params[key];
+
+			if (value || operator === 'filter') {
+				result[operator] = method.call(this, value, params);
+			}
+		}, this);
+
+		return result as QueryOptions;
+	}
+
+	/**
+	 * parses query string/object to InfluxDB query
+	 * @param {string | Object} query
+	 * @return {string}
+	 */
+	parseQuery(query: string | Record<string, any>): string {
+		const result = this.parse(query);
+		return this.createQuery(result);
+	}
+
+	/**
+	 * create AQL query from QueryOptions
+	 * @param qo QueryOptions
+	 * @return {string} AQL query
+	 */
+	createQuery(qo: QueryOptions): string {
+		const options = this._options;
+		let result = 'SELECT ' + (qo.aggregate?.agg || qo.fields || '*') + ' FROM ' + options.measurements;
+		result += qo.filter.filters ? ' ' + qo.filter.filters : '';
+		result += qo.sort ? ' ' + qo.sort : '';
+		result += qo.limit ? ' ' + qo.limit : '';
+		result += qo.aggregate && qo.aggregate.groupBy ? ' ' + qo.aggregate.groupBy : '';
+		return result;
+	}
+
+	/**
+	 * parses string to typed values
+	 * This methods will apply auto type casting on Number, RegExp, Date, Boolean and null
+	 * Also, it will apply defined casters in given options of the instance
+	 * @param {string} value
+	 * @param {string} key
+	 * @return {any} typed value
+	 */
+	parseValue(value: string, key?: string): any {
+		const options = this._options;
+
+		// Apply casters
+		// Match type casting operators like: string(true), _caster(123), $('test')
+		const casters = options.casters;
+		const casting = value.match(/^([a-zA-Z_$][0-9a-zA-Z_$]*)\((.*)\)$/);
+		if (casting && casters[casting[1]]) {
+			return casters[casting[1]](casting[2]);
+		}
+
+		// Apply casters per params
+		if (key && options.castParams && options.castParams[key] && casters[options.castParams[key]]) {
+			return casters[options.castParams[key]](value);
+		}
+
+		// cast array
+		if (value.includes(',')) {
+			return value.split(',').map(val => this.parseValue(val, key));
+		}
+
+		// Apply type casting for Number, RegExp, Date, Boolean and null
+		// Match regex operators like /foo_\d+/i
+		const regex = value.match(/^\/(.*)\/(i?)$/);
+		if (regex) {
+			return new RegExp(regex[1], regex[2]);
+		}
+
+		// Match boolean values
+		if (value === 'true') {
+			return true;
+		}
+		if (value === 'false') {
+			return false;
+		}
+
+		// Match null
+		if (value === 'null') {
+			return null;
+		}
+
+		// Match numbers (string padded with zeros are not numbers)
+		if (value !== '' && !isNaN(Number(value)) && !/^0[0-9]+/.test(value)) {
+			return Number(value);
+		}
+
+		return value;
+	}
+
+	castFilter(filter, params): any {
+		const options = this._options;
+		const parsedFilter = filter ? this.parseFilter(filter) : {};
+		return (
+			Object.keys(params)
+				.map(val => {
+					const join = params[val] ? `${val}=${params[val]}` : val;
+					// Separate key, operators and value
+					const [, prefix, key, op, value] = join.match(/(!?)([^><!=]+)([><=]=?|!?=|)(.*)/);
+					return { prefix, key, op: this.parseOperator(op), value: this.parseValue(value, key) };
+				})
+				.filter(({ key }) => !options.whitelist || options.whitelist.indexOf(key) > -1)
+				.filter(({ key }) => options.blacklist.indexOf(key) === -1)
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				.reduce((result, { prefix, key, op, value }) => {
+					if (Array.isArray(value)) {
+						result.filters = typeof result.filters == 'string' ? result.filters + ' AND ' : 'WHERE ';
+						if (op == '!=') {
+							result.filters +=
+								'(' + value.reduce((acc, curr) => (acc ? acc + ' AND ' : '') + key + ' != ' + escape.stringLit(curr), undefined) + ')';
+							return result;
+						} else {
+							result.filters +=
+								'(' + value.reduce((acc, curr) => (acc ? acc + ' OR ' : '') + key + ' = ' + escape.stringLit(curr), undefined) + ')';
+							return result;
+						}
+					} else if (value instanceof RegExp) {
+						op = op == '!=' ? '!~' : '=~';
+					}
+					result.filters = typeof result.filters == 'string' ? result.filters + ' AND ' : 'WHERE ';
+					if (typeof value == 'number') {
+						result.filters += `${key} ${op} ${value}`;
+					} else {
+						result.filters += `${key} ${op} ${escape.stringLit(value)}`;
+					}
+					return result;
+				}, parsedFilter)
+		);
+	}
+
+	parseFilter(filter) {
+		try {
+			if (typeof filter === 'object') {
+				return filter;
+			}
+			return JSON.parse(filter);
+		} catch (err) {
+			throw new Error(`Invalid JSON string: ${filter}`);
+		}
+	}
+
+	parseOperator(operator) {
+		if (operator === '=') {
+			return '=';
+		} else if (operator === '!=') {
+			return '!=';
+		} else if (operator === '>') {
+			return '>';
+		} else if (operator === '>=') {
+			return '>=';
+		} else if (operator === '<') {
+			return '<';
+		} else if (operator === '<=') {
+			return '<=';
+		} else if (!operator) {
+			return '!';
+		}
+	}
+
+	/**
+	 * cast fields query to list of fields
+	 * fields=email,phone
+	 * =>
+	 * email,phone
+	 * @param val
+	 */
+	castFields(val): string {
+		const options = this._options;
+		const result = val
+			.split(',')
+			.filter(field => !options.whitelist || options.whitelist.indexOf(field) > -1)
+			.filter(field => options.blacklist.indexOf(field) === -1);
+		return result.join(',');
+	}
+
+	/**
+	 * cast sort query to string
+	 * sort=-firstName
+	 * =>
+	 * ORDER BY firstName DESC
+	 *
+	 * @param sort
+	 */
+	castSort(sort: string) {
+		const arr = _.isString(sort) ? sort.split(',') : sort;
+		const r: Array<any> = arr.map(x => x.match(/^(\+|-)?(.*)/));
+
+		return r.reduce((result, [, dir, key]) => {
+			if (key && /^[a-zA-Z0-9_]*$/.test(key)) {
+				result = (_.isString(result) && result != '' ? result + ', ' : 'ORDER BY ') + key.trim() + (dir === '-' ? ' DESC' : '');
+			}
+			return result;
+		}, '');
+	}
+
+	/**
+	 * cast aggregate query to string
+	 * aggregate=country,city:totalPrice sum price,averagePrice avg price,priceCount count price
+	 * =>
+	 * COLLECT country=o.country, city = o.city
+	 * AGGREGATE totalPrice = SUM(o.price), averagePrice = AVG(o.price), priceCount = COUNT(o.price)
+	 * @param aggregate
+	 */
+	castAggregate(aggregate: string) {
+		let [collFields, aggregations] = aggregate.split(':', 2);
+		let groupBy;
+		let agg;
+		const types = [
+			'sum',
+			'count',
+			'distict',
+			'integral',
+			'mean',
+			'median',
+			'mode',
+			'spread',
+			'stddev',
+			'bottom',
+			'first',
+			'last',
+			'max',
+			'min',
+			'percentile',
+			'sample',
+			'top',
+		];
+
+		if (!collFields && !aggregations) {
+			return '';
+		}
+
+		if (!aggregations) {
+			aggregations = collFields;
+			collFields = '';
+		}
+
+		for (const field of collFields.split(',')) {
+			if (field && /^[a-zA-Z0-9_]*$/.test(field)) {
+				groupBy = (groupBy ? groupBy + ', ' : 'GROUP BY ') + field;
+			}
+		}
+		for (const a of aggregations.split(',')) {
+			const [as, type, field] = a.split(' ');
+			if (as == 'time') {
+				groupBy = (groupBy ? groupBy + ', time(' : 'GROUP BY ') + type + ')';
+			} else if (as && type && field && /^[a-zA-Z0-9_]*$/.test(field) && types.includes(type)) {
+				agg = (agg ? agg + ', ' : '') + type.toUpperCase() + '(' + field + ') AS ' + as;
+			}
+		}
+
+		return { groupBy, agg };
+	}
+
+	/**
+	 * cast limit query to object like
+	 * limit=50,100
+	 * =>
+	 * LIMIT 50 OFFSET 100
+	 * @param limit
+	 */
+	castLimit(limit: string) {
+		const matches = limit.match(/^([0-9]*),?([0-9]*)$/);
+		let result = '';
+		if (matches) {
+			result = matches[1] != '' ? 'LIMIT ' + matches[1] : '';
+			result += matches[2] != '' ? ' OFFSET ' + matches[2] : '';
+		}
+		return result;
+	}
+}
---
+++ src/test.spec.ts
@@ -0,0 +1,140 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { suite, test } from '@testdeck/mocha';
+import { assert } from 'chai';
+
+import { InfluxDbQueryParser } from './';
+
+@suite('Tester')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class Tester {
+	@test('should parse general query')
+	generalParse1() {
+		const parser = new InfluxDbQueryParser();
+		const qry =
+			'date=2016-01-01&boolean=true&integer=10&regexp=/foobar/i&null=null&startTime>2020-06-16&type=note,task&limit=,10&sort=type,-startTime&fields=startTime,endTime';
+		const parsed = parser.parse(qry);
+		assert.isNotNull(parsed.filter);
+	}
+
+	@test('should parse more complex query')
+	generalParse2() {
+		const parser = new InfluxDbQueryParser();
+		const parsed = parser.parse('timestamp>2017-10-01&timestamp<2020-01-01&author.firstName=/frederick/i&limit=50,100&sort=-timestamp&fields=name');
+		assert.strictEqual(parsed.filter.filters, "WHERE timestamp > '2017-10-01' AND timestamp < '2020-01-01' AND author.firstName =~ '/frederick/i'");
+		assert.isOk(parsed.limit == 'LIMIT 50 OFFSET 100');
+		assert.isNotNull(parsed.sort);
+		assert.isNotNull(parsed.fields);
+	}
+
+	@test('should parse multiple option query')
+	generalParse3() {
+		const parser = new InfluxDbQueryParser();
+		const qry = 'author=William,Frederick,Durst';
+		const parsed = parser.parse(qry);
+		assert.strictEqual(parsed.filter.filters, "WHERE (author = 'William' OR author = 'Frederick' OR author = 'Durst')");
+	}
+
+	@test('should parse built in casters')
+	builtInCastersTest() {
+		const parser = new InfluxDbQueryParser();
+		const qry = 'key1=string(10)&key2=date(2017-10-01)&key3=string(null)&key4=123';
+		const parsed = parser.parse(qry);
+		assert.strictEqual(parsed.filter.filters, "WHERE key1 = '10' AND key2 = '2017-10-01T00:00:00.000Z' AND key3 = 'null' AND key4 = 123");
+	}
+
+	@test('should parse only whitelist fields')
+	parseWhitelist() {
+		const parser = new InfluxDbQueryParser({ whitelist: ['firstName', 'lastName'] });
+		const parsed = parser.parse('firstName=William&middleName=Frederick&lastName=Durst&password=secret');
+		assert.strictEqual(parsed.filter.filters, "WHERE firstName = 'William' AND lastName = 'Durst'");
+	}
+
+	@test('should not parse blacklisted fields')
+	parseBlacklist() {
+		const parser = new InfluxDbQueryParser({ blacklist: ['middleName', 'password'] });
+		const parsed = parser.parse('firstName=William&middleName=Frederick&lastName=Durst&password=secret');
+		assert.strictEqual(parsed.filter.filters, "WHERE firstName = 'William' AND lastName = 'Durst'");
+	}
+
+	@test('should create equal query for filters')
+	parseQuery1() {
+		const parser = new InfluxDbQueryParser({ measurements: 'events' });
+		const parsed = parser.parse('startTime>2020-06-16&private=false');
+		const query = parser.createQuery(parsed);
+		assert.equal(query, "SELECT * FROM events WHERE startTime > '2020-06-16' AND private = 'false'");
+	}
+
+	@test('should create equal query for filters, limit and sort')
+	parseQuery2() {
+		const parser = new InfluxDbQueryParser({ measurements: 'events' });
+		const parsed = parser.parse('startTime>2020-06-16&private=false&limit=10&sort=startTime');
+		const query = parser.createQuery(parsed);
+		assert.equal(query, "SELECT * FROM events WHERE startTime > '2020-06-16' AND private = 'false' ORDER BY startTime LIMIT 10");
+	}
+
+	@test('should create query only for whitelisted fields')
+	parseQueryWhiteList() {
+		const parser = new InfluxDbQueryParser({ measurements: 'users', whitelist: ['firstName', 'lastName'] });
+		const parsed = parser.parse('fields=firstName,middleName,lastName,password');
+		const query = parser.createQuery(parsed);
+		assert.equal(query, 'SELECT firstName,lastName FROM users');
+	}
+
+	@test('should not create query for blacklisted fields')
+	parseQueryBlacklist() {
+		const parser = new InfluxDbQueryParser({ measurements: 'users', blacklist: ['middleName', 'password'] });
+		const parsed = parser.parse('fields=firstName,middleName,lastName,password');
+		const query = parser.createQuery(parsed);
+		assert.equal(query, 'SELECT firstName,lastName FROM users');
+	}
+
+	@test('should create grouped aggregates')
+	parseAggregate1() {
+		const parser = new InfluxDbQueryParser({ measurements: 'deals' });
+		const parsed = parser.parse('aggregate=owner,status:totalPrice sum price,averagePrice mean price,priceCount count price');
+		const query = parser.createQuery(parsed);
+		assert.equal(query, 'SELECT SUM(price) AS totalPrice, MEAN(price) AS averagePrice, COUNT(price) AS priceCount FROM deals GROUP BY owner, status');
+	}
+
+	@test('should create total aggregates')
+	parseAggregate2() {
+		const parser = new InfluxDbQueryParser({ measurements: 'deals' });
+		const parsed = parser.parse('aggregate=totalPrice sum price,averagePrice mean price,priceCount count price');
+		const query = parser.createQuery(parsed);
+		assert.equal(query, 'SELECT SUM(price) AS totalPrice, MEAN(price) AS averagePrice, COUNT(price) AS priceCount FROM deals');
+	}
+
+	@test('should create time aggregates')
+	parseAggregate3() {
+		const parser = new InfluxDbQueryParser({ measurements: 'deals' });
+		const parsed = parser.parse('aggregate=owner:time 5m,totalPrice sum price,averagePrice mean price,priceCount count price');
+		const query = parser.createQuery(parsed);
+		assert.equal(query, 'SELECT SUM(price) AS totalPrice, MEAN(price) AS averagePrice, COUNT(price) AS priceCount FROM deals GROUP BY owner, time(5m)');
+	}
+
+	@test('should parse date shortcuts')
+	parseDateShortcuts1() {
+		const parser = new InfluxDbQueryParser({ measurements: 'deals' });
+		const parsed = parser.parse(
+			'thisYearStarts=date(startOfYear)&thisYearEnds=date(endOfYear)&thisMonthStarts=date(startOfMonth)&thisMonthEnds=date(endOfMonth)&thisQuarterStarts=date(startOfQuarter)&thisQuarterEnds=date(endOfQuarter)'
+		);
+		const query = parser.createQuery(parsed);
+		assert.equal(
+			query,
+			"SELECT * FROM deals WHERE thisYearStarts = '2020-01-01T00:00:00.000Z' AND thisYearEnds = '2020-12-31T23:59:59.999Z' AND thisMonthStarts = '2020-09-01T00:00:00.000Z' AND thisMonthEnds = '2020-09-30T23:59:59.999Z' AND thisQuarterStarts = '2020-07-01T00:00:00.000Z' AND thisQuarterEnds = '2020-09-30T23:59:59.999Z'"
+		);
+	}
+
+	@test('should parse date shortcuts with modifiers')
+	parseDateShortcuts2() {
+		const parser = new InfluxDbQueryParser({ measurements: 'deals' });
+		const parsed = parser.parse(
+			'previousYearStarts=date(startOfYear:-1)&previousYearEnds=date(endOfYear:-1)&nextMonthStarts=date(startOfMonth:1)&nextMonthEnds=date(endOfMonth:1)'
+		);
+		const query = parser.createQuery(parsed);
+		assert.equal(
+			query,
+			"SELECT * FROM deals WHERE previousYearStarts = '2019-01-01T00:00:00.000Z' AND previousYearEnds = '2019-12-31T23:59:59.999Z' AND nextMonthStarts = '2020-10-01T00:00:00.000Z' AND nextMonthEnds = '2020-10-30T23:59:59.999Z'"
+		);
+	}
+}
